c64d93e9a4df16b0490c8c5e99ecdc7cf776eb18
mitsece <166103244+mitsece@users.noreply.github.com>
2024-05-13 20:18:46 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_mitssdd (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,26 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:6]=2'b0; // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+     co_processor co_processor  (
+    .clk(clk),
+    .reset(rst_n),
+	.r0(ui_in),
+	.check(uio_in[1:0]),
+	.Q(uo_out[0])
+	
+);
+    fault_pro fault_pro  (
+    .clk(clk),
+    .reset(rst_n),
+	.r0(ui_in),
+	.check(uio_in[1:0]),
+	.out(uo_out[3:1]),
+	.out1(uo_out[5:4])
+		
+	
+);
 
 endmodule
